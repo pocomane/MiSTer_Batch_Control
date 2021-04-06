2e551a61cfd91132d4dca1479e132dae44e1413b
pocomane <none@mail.com>
2021-04-06 18:01:49 +0200

    Proper link in the mister_misc package

--- build.sh
+++ build.sh
@@ -24,8 +24,9 @@ cd build
 arm-linux-musleabihf-gcc -std=c99 -D_XOPEN_SOURCE=700 -static -O2 -o mbc ../mbc.c ||die
 arm-linux-musleabihf-strip mbc ||die
 mkdir -p hook/expose ||die
-echo "#!/bin/bash" > "hook/expose/__unnamed__" ||die
-echo "./mbc \$@"  >> "hook/expose/__unnamed__" ||die
-tar -czf mbc.tar.gz mbc ./hook/action/__unnamed__
+cd hook/expose ||die
+ln -s ../../mbc __unnamed__ ||die
+cd -
+tar -czf mbc.tar.gz mbc hook/expose/__unnamed__
 cd ..
 
